f09e82f53c67a2e1820ec5de0795c092737f1c65
Roydano <roy.dano90@gmail.com>
2021-07-16 15:16:39 +0200

    impostazione header top struttura base senza commenti

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,13 +1,79 @@
 <template>
-  <div></div>
+  <div class="header_top">
+    <div class="my_container">
+      <div class="row">
+        <div class="col d-flex justify-content-between">
+          <div class="lef_side">
+            <SocialIcon/>
+          </div>
+          <div class="right_side">
+            <ul>
+              <li>Sopping Cart</li>
+              <li>My Account<i class="fas fa-chevron-down"></i></li>
+              <li><i class="fas fa-shopping-cart"></i>Cart<i class="fas fa-chevron-down"></i></li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SocialIcon from '@/components/SocialIcon.vue'
+
 export default {
+  name: 'HeaderTop',
+  components:{
+    SocialIcon
+  }
 
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  .header_top{
+    height: 50px;
+    line-height: 50px;
+    border-bottom: 1px solid lightgrey;
+  }
+
+  .my_container{
+    max-width: 70%;
+    margin: 0 auto;
+
+    li{
+      display: inline-block;
+      padding: 0 15px;
+      border-right: 1px solid lightgrey;
+      font-size: 14px;
+
+      &:last-child{
+        border-right: none;
+        margin-left: 70px;
+        text-transform: uppercase;
+
+        i{
+          margin-right: 10px;
+          font-size: 12px;
+
+          &:last-child{
+            margin-left: 10px;
+            font-size: 16px;
+          }
+        }
+      }
+
+      i{
+        margin-right: 0;
+        margin-left: 15px;
+        font-size: 10px;
+      }
+    }
+  }
+
+  
 
 </style>
---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="social_icon">
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-youtube"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lanh="scss">
+
+    i{
+        margin-right: 30px;
+        color: #575c61;
+    }
+
+</style>
