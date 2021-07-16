6b53d264cc21c2dc9f68db1d0e082a4261eb13d0
Roydano <roy.dano90@gmail.com>
2021-07-16 15:29:10 +0200

    add header top con commenti

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -3,17 +3,21 @@
     <div class="my_container">
       <div class="row">
         <div class="col d-flex justify-content-between">
+          <!-- inizio sezione di sinistra con icone social -->
           <div class="lef_side">
             <SocialIcon/>
           </div>
+          <!-- fine sezione di sinistra con icone social -->
+
+          <!-- inizio sezione destra con azioni shop utente -->
           <div class="right_side">
             <ul>
               <li>Sopping Cart</li>
               <li>My Account<i class="fas fa-chevron-down"></i></li>
               <li><i class="fas fa-shopping-cart"></i>Cart<i class="fas fa-chevron-down"></i></li>
             </ul>
-
           </div>
+          <!-- inizio sezione destra con azioni shop utente -->
         </div>
       </div>
     </div>
@@ -33,32 +37,39 @@ export default {
 </script>
 
 <style lang="scss">
-
   .header_top{
     height: 50px;
     line-height: 50px;
     border-bottom: 1px solid lightgrey;
   }
 
+  // formattazione contenitore con larghezza massima per lasciare spazio ai lati
   .my_container{
     max-width: 70%;
     margin: 0 auto;
 
+    // formattazione lista sezione di destra
     li{
       display: inline-block;
       padding: 0 15px;
       border-right: 1px solid lightgrey;
       font-size: 14px;
+      color: #a0a5a5;
 
+      // formattazione per ultimo elemento della lista
       &:last-child{
         border-right: none;
-        margin-left: 70px;
+        margin-left: 50px;
         text-transform: uppercase;
+        color: #575c61;
 
+        // formattazione per carrello
         i{
           margin-right: 10px;
           font-size: 12px;
+          color: #575c61;
 
+          // formattazione per freccia cart
           &:last-child{
             margin-left: 10px;
             font-size: 16px;
@@ -66,10 +77,13 @@ export default {
         }
       }
 
+      // formattazione freccia my account
       i{
         margin-right: 0;
         margin-left: 15px;
         font-size: 10px;
+        color: #a0a5a5;
+
       }
     }
   }
