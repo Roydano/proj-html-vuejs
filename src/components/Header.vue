ef19c8a997a8b9aa5ab6aaeb8cc7557f3daa1763
Roydano <roy.dano90@gmail.com>
2021-07-16 14:19:37 +0200

    add componenti header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,33 @@
 <template>
-  <header></header>
+  <header>
+      <!-- inizio header top -->
+      <HeaderTop/>
+      <!-- fine header top -->
+
+      <!-- inizio header bottom -->
+      <HeaderBottom/>
+      <!-- fine header bottom -->
+
+      <!-- inizio jumbotron -->
+      <Jumbotron/>
+      <!-- fine jumbotron -->
+
+  </header>
 </template>
 
 <script>
+import HeaderTop from '@/components/HeaderTop.vue'
+import HeaderBottom from '@/components/HeaderBottom.vue'
+import Jumbotron from '@/components/Jumbotron.vue'
+
+
 export default {
+    name: 'Header',
+    components:{
+        HeaderTop,
+        HeaderBottom,
+        Jumbotron
+    }
 
 }
 </script>
